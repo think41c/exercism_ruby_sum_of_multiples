f0daf3ffd47d333006c181475551d878cfd0b8b9
Tony DiNitto <think41c@gmail.com>
2015-04-07 17:05:33 -0600

    Initialize method set, * operator used. Logic needs to be implemented.

--- sum_of_multiples.rb
+++ sum_of_multiples.rb
@@ -1,6 +1,13 @@
 class SumOfMultiples
 
+  def initialize (default = 5, *multiple_divisor)
+    p default
+    p multiple_divisor
+  end
+
   def self.to(x)
     0
   end
 end
+
+SumOfMultiples.new(5, 4, 9)
