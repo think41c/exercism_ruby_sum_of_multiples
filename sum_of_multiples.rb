24fcac8cdc51de1b17f4538a3d49b44868f4f52f
Tony DiNitto <think41c@gmail.com>
2015-04-07 17:19:17 -0600

    Framework of methods and their responsibility laid out.

--- sum_of_multiples.rb
+++ sum_of_multiples.rb
@@ -1,12 +1,22 @@
 class SumOfMultiples
 
   def initialize (first_multiple = 3, second_multiple = 5, *additional_multiple)
-    
+
   end
 
   def self.to(x)
+    find_the_multiples
+    sum_the_multiples
+  end
+
+  def self.find_the_multiples
+
+  end
+
+  def self.sum_the_multiples
     0
   end
+
 end
 
 SumOfMultiples.new(9)
